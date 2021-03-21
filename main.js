ca56f933684d8f5e038bb6dc41db9136aa118b80
GomaNiroula <goma.niroula@ucdenver.edu>
2021-03-20 22:09:25 -0600

    MongoDB Classwork Added

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const express = require("express"),
 app = express(),
 homeController = require("./controllers/homeController"),
 errorController = require("./controllers/errorController"),
-layouts = require("express-ejs-layouts");
+subscribersController = require("./controllers/subscribersController"),
+layouts = require("express-ejs-layouts"), mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/confetti_cuisine",
+    { useNewUrlParser: true });
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
@@ -20,8 +24,11 @@ app.use(
 app.use(express.json());
 
 app.get("/courses", homeController.showCourses);
-app.get("/contact", homeController.showSignUp);
-app.post("/contact", homeController.postedSignUpForm);
+app.get("/subscribers", subscribersController.getAllSubscribers);
+app.get("/contact", subscribersController.getSubscriptionPage);
+app.post("/subscribe", subscribersController.saveSubscriber);
+// app.get("/contact", homeController.showSignUp);
+// app.post("/contact", homeController.postedSignUpForm);
 
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
